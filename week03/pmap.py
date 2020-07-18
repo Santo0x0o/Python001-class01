4a873b15fd122750ec47abc341c5079119892093
Santo <santo0x0o@gmail.com>
2020-07-19 00:56:20 +0800

    week03 homework1 update

--- week03/pmap.py
+++ week03/pmap.py
@@ -8,7 +8,7 @@ import os
 import subprocess
 import ipaddress
 import socket
-from  multiprocessing import Pool, Lock
+from multiprocessing import Pool, Lock
 from concurrent.futures import ThreadPoolExecutor as executor
 import functools
 import json
@@ -19,7 +19,7 @@ import time
 # -f ping：进行 ping 测试
 # -f tcp：进行 tcp 端口开放、关闭测试。
 # -ip：连续 IP 地址支持 192.168.0.1-192.168.0.100 写法。
-# -p: tcp测试目标端口，支持0-1024; 80,443,139; 
+# -p: tcp测试目标端口，支持0-1024; 80,443,139;
 # -w：扫描结果进行保存。
 
 
@@ -27,6 +27,7 @@ class UserInputError(Exception):
     def __init__(self, ErrorInfo):
         super().__init__(self, ErrorInfo)
         self.errorinfo = ErrorInfo
+
     def __str__(self):
         return self.errorinfo
 
@@ -34,48 +35,57 @@ class UserInputError(Exception):
 # print_lock =Lock()
 
 def validate_port(p):
-    if 0<p<65536:
-        return(p)      
+    if 0 < p < 65536:
+        return(p)
     else:
         raise UserInputError(f'ValueError: {p} does not appear to be a port')
+
 
 def validate_ip(ip):
     try:
         valid_ipv4 = ipaddress.IPv4Address(ip)
-        return(valid_ipv4)      
+        return(valid_ipv4)
     except ValueError:
-        raise UserInputError(f'ValueError: {ip} does not appear to be an IP address')
+        raise UserInputError(
+            f'ValueError: {ip} does not appear to be an IP address')
 
-def generate_ip_list(s,e):
-    for ips in ipaddress.summarize_address_range(ipaddress.IPv4Address(s),ipaddress.IPv4Address(e)):
+
+def generate_ip_list(s, e):
+    for ips in ipaddress.summarize_address_range(ipaddress.IPv4Address(s), ipaddress.IPv4Address(e)):
         # 生成ip网段
         # print(ips)
         yield from ips
 
-### formalize the port
+# formalize the port
+
+
 def port_init(args):
-    args.p=(None if args.f=='ping' else args.p )
-    port_list=[]
-    if args.f=='tcp' and args.p is None:
+    args.p = (None if args.f == 'ping' else args.p)
+    port_list = []
+    if args.f == 'tcp' and args.p is None:
         print('usage: pmap.py [-h] -f {ping,tcp} -ip IP [-p P] [-n N] [-w W]')
-        raise UserInputError('pmap.py: error: the following argument -p is mandatory for tcp test.')
-    elif args.f=='tcp' and args.p ==[]:
-        args.p=['1-1024']
-        port_list=[p for p in range(1,1025)]
-    elif args.f=='tcp' and args.p !=[]:
+        raise UserInputError(
+            'pmap.py: error: the following argument -p is mandatory for tcp test.')
+    elif args.f == 'tcp' and args.p == []:
+        args.p = ['1-1024']
+        port_list = [p for p in range(1, 1025)]
+    elif args.f == 'tcp' and args.p != []:
         for p in args.p:
             port_list.append(validate_port(p))
     return port_list
 
-### formalize the ip addresss
+# formalize the ip addresss
+
+
 def ip_init(args):
     ip_list = []
-    if '-' in args.ip[0] and len(args.ip)>1:
+    if '-' in args.ip[0] and len(args.ip) > 1:
         # print('usage: pmap.py [-h] -f {ping,tcp} -ip IP [-p P] [-n N] [-w W]')
-        raise UserInputError('pmap.py: error: input the following argument -ip value in wrong format.')
-    elif '-' in args.ip[0] and len(args.ip)==1:
+        raise UserInputError(
+            'pmap.py: error: input the following argument -ip value in wrong format.')
+    elif '-' in args.ip[0] and len(args.ip) == 1:
         ip_s, ip_e = args.ip[0].split('-')
-        ip_list=list(generate_ip_list(ip_s, ip_e))
+        ip_list = list(generate_ip_list(ip_s, ip_e))
     else:
         for ip in args.ip:
             ip_list.append(validate_ip(ip))
@@ -85,76 +95,87 @@ def ip_init(args):
 def ping_func(ip):
     # print('start ping')
     result = subprocess.run(f'ping {ip} -n 3', stdout=subprocess.DEVNULL)
-    if result.returncode==0:
+    if result.returncode == 0:
         # with print_lock:
         print(f'ping {ip} successed.')
-        return {str(ip):1}
+        return {str(ip): 1}
     else:
         # with print_lock:
         print(f'ping {ip} failed.')
-        return {str(ip):0}
+        return {str(ip): 0}
 
 
 def tcp_func(ip, port):
-    t=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     t.settimeout(3)
     try:
-        if t.connect_ex((ip, port))==None:
+        if t.connect_ex((ip, port)) == None:
             print(f'{port} of {ip} is Up')
-            return {str(ip)+"_"+str(port):1}
+            return {str(ip)+"_"+str(port): 1}
     except ConnectionRefusedError:
         print(f'{port} of {ip} is Down')
-        return {str(ip)+"_"+str(port):0}
-    
+        return {str(ip)+"_"+str(port): 0}
+
 
 def pmap(args):
     st = time.time()
     n = 2 if args.n == None else args.n
     host = ip_init(args)
     port = port_init(args)
-    output_dict={}
-    output=[]
+    output_dict = {}
+    output = []
 
-    if args.m=='proc':
-        if args.f=='ping':            
+    if args.m == 'proc':
+        if args.f == 'ping':
             pool = Pool(n)
             output = pool.map(ping_func, host)
             pool.close()
             pool.join()
 
-        if args.f=='tcp':  
-            for i in host :
+        if args.f == 'tcp':
+            for i in host:
                 pool = Pool(n)
-                output +=pool.map(functools.partial(tcp_func, str(i)), port)
+                output += pool.map(functools.partial(tcp_func, str(i)), port)
                 pool.close()
                 pool.join()
-    if args.m=='thread':
+    if args.m == 'thread':
         with executor(n) as pool:
-            if args.f=='ping':    
+            if args.f == 'ping':
                 output = pool.map(ping_func, host)
 
-            if args.f=='tcp':
-                for i in host :
-                    output +=pool.map(functools.partial(tcp_func, str(i)), port)
+            if args.f == 'tcp':
+                for i in host:
+                    output += pool.map(functools.partial(tcp_func,
+                                                         str(i)), port)
     if args.w:
         for l in output:
             output_dict.update(l)
         ouput_json = json.dumps(output_dict, indent=4, sort_keys=True)
         t = time.strftime('%Y%m%d%H%M%S', time.localtime())
-        with open('./'+args.f+'_test_result_'+t+'.json','wt') as f:
+        with open('./'+args.f+'_test_result_'+t+'.json', 'wt') as f:
             f.write(ouput_json)
         print('output saved in json file:./'+args.f+'_test_result_'+t+'.json')
     et = time.time()
     if args.v:
         print(f'Scanning spent {str(round(et-st,3))} s')
-if __name__=='__main__': 
-    argparser = argparse.ArgumentParser(description='-'*25+'Help info of pmap tool'+'-'*25)
-    argparser.add_argument('-f', type=str, required=True, choices=['ping','tcp'], help='"ping" for ping test; "tcp" for port connectinity test.')
-    argparser.add_argument('-ip', type=str, nargs='+', required=True, help='ipV4 address, support single or patch in format "192.168.0.1-192.168.0.100"; "192.168.0.1 192.168.0.3".')
-    argparser.add_argument('-p', type=int, nargs='*', help='port mandatory for tcp test, support single or patch in format "1-1024"(default); "80 443" .')
-    argparser.add_argument('-m', type=str, choices=['proc', 'thread'], required=True, help='select to run by multiprocess or multithread.')
-    argparser.add_argument('-n', type=int, choices=[2, 4, 8], help='Num of multi job.')
-    argparser.add_argument('-w', action='store_true', help='Save the scan result to current folder.')
-    argparser.add_argument('-v', action='store_true', help='Show the time spending for scan test.')
-    args=argparser.parse_args()
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(
+        description='-'*25+'Help info of pmap tool'+'-'*25)
+    argparser.add_argument('-f', type=str, required=True, choices=[
+                           'ping', 'tcp'], help='"ping" for ping test; "tcp" for port connectinity test.')
+    argparser.add_argument('-ip', type=str, nargs='+', required=True,
+                           help='ipV4 address, support single or patch in format "192.168.0.1-192.168.0.100"; "192.168.0.1 192.168.0.3".')
+    argparser.add_argument('-p', type=int, nargs='*',
+                           help='port mandatory for tcp test, support single or patch in format "1-1024"(default); "80 443" .')
+    argparser.add_argument('-m', type=str, choices=['proc', 'thread'],
+                           required=True, help='select to run by multiprocess or multithread.')
+    argparser.add_argument(
+        '-n', type=int, choices=[2, 4, 8], help='Num of multi job.')
+    argparser.add_argument('-w', action='store_true',
+                           help='Save the scan result to current folder.')
+    argparser.add_argument('-v', action='store_true',
+                           help='Show the time spending for scan test.')
+    args = argparser.parse_args()
     pmap(args)
